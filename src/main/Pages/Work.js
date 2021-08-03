e30e216d5c6294c3d0840da0d28c1b891c87f835
Pape Momar Ndiaye <pmomar44@gmail.com>
2021-08-03 08:57:42 +0000

    Heroku Test 3

--- src/main/Pages/Work.js
+++ src/main/Pages/Work.js
@@ -1,6 +1,10 @@
 import { React, useState, useEffect } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import GeekBlog1 from '../Style/Image/Geek Blog - Google Chrome 11_07_2021 11_46_50.png';
+import GeekBlog2 from '../Style/Image/Geek Blog - Google Chrome 11_07_2021 11_47_04.png';
+import GeekBlog3 from '../Style/Image/Geek Blog - Google Chrome 11_07_2021 11_48_41.png';
+
 
 const WorkPresentation = ({ parentCallback, theTitle, theDescription, theImagesToPresentation, gotALinkOrNot, theLink }) => {
     console.log(gotALinkOrNot)
@@ -54,6 +58,7 @@ const OneStuffsContainer = ({ theId, title, parentCallback, description, Images,
         let stuff = document.querySelector(`#${theId}`)
         stuff.style.backgroundImage = `url(${Images[0]})`
     })
+
     return (
         <div key={theId} onClick={() => parentCallback(title, description, Images, gotALink, theLink)} className="stuffs_contain">
             <div className="stuffs">
@@ -101,11 +106,7 @@ const Work = () => {
             <div className="web_work ">
                 <OneStuffsContainer parentCallback={clickOnWork} theId="Geek-Blog" title="Geek Blog" gotALink={true}
                     description={`Cette platform est mon premier projet M.E.R.N. Il s'agit d'un réseau social ou il est possible de fair des posts de comanter des posts et de chater avec des amis.
-ps: le code est en open-source sur mon Github`} theLink={'https://geek--blog.herokuapp.com/SignUp'} Images={["https://previews.dropbox.com/p/thumb/ABM5-sgid-GwgpImXkyytq_mQ3Kfjbre3S4zGkDi7neQcijfDMQDFLeoQvrQY4eDggqEns9gEbMM1H5ooRGi1ym11BcCVBWHVD9SASpPar5pW57KRqmr3Lwm3GjQmSqc4mEqNx8AHZedT9AG_sBjzH_jZJK1bswl4vFJR8JKdrl91zpcCmaV0d3_aEZbJaH5h5UPTF6_8V84zQ0rI5_19QKWgJwRakeFHI9Rz6RjRXLnQNMqkxN4_gpKXIlQS5H7loYMFAlZa3bF02ZNb8AL-c2I2ymcpMuv20ApDWG9h9mGELCFMXb5TpyW34LSumjX-lOfosIZD3RRdTRnSvdyAgNK6idbc1Bfz9aCmPal53eEuQ/p.png?fv_content=true&size_mode=5",
-                        `https://previews.dropbox.com/p/thumb/ABN4o936XFy8FmJ6yjADpPT2HVPKdNFqkbbN0Gbj4kjs49-y6jVXUQyqa92ruLfqOwjA20Z5Oa2s3INCCwoGEGSqWzCmuZ1U06a9y1cq0gq--b4DtOFgmUlTPKPFT1vSZjaV9GhY5zPqTZ5tNve3qJS3EojbXfOJ7ERvCAR7bHBBHSscL66CdZK3vBOwo5f7E1xCDE7hL8RJmJvgRonFJNdcMD8-tnM17GH-bZfQ2XvwQmRWmyRagIMmE73f36B-v2YBLkRMwo6tb7NLIKAZhFwUs9eks99Dmb7n46PiJMJA5PxWfkJdpAtCSWz2ZDfQp9TBMcnNK-LDTuIrp1KZzTOV4Ipb_HOkpyXh6AH3zrSOAw/p.png?fv_content=true&size_mode=5`,
-                        `https://previews.dropbox.com/p/thumb/ABMdzXJHJoLSPJxIxB7RM3_6T6mSmoJNGMWP8kgCp3hWvdRvoIkOX2KfyJYxBGAyobDc-j8dBQOfwZmELWZ1ncdLc61S1L16mgqAA_btaOSepGCvOoOalN_X2zzYhzLW1pZtuDBDGxvSv2NniRiwJDskSJCL-vF64PR6mwRk4Y5qM5ZerDWUr1uDxbKx9v9k_Mo7XvEdVwxvaXUgPW7Qz8Zd-YEcRDr7pyKkLD1nZ2u1Phs1sQMfmSAVhP526m2ww2OccDV6ekGZ5XDza1DhPrlr9lAPL7T7mxktq78chyYvgguHwVqYz8r3U6hZtIY9K0xeZ0FyB357e0F8U63hSQ7_HHYX85uLwwxNoJLXhGxRZA/p.png?fv_content=true&size_mode=5`,
-                        `https://previews.dropbox.com/p/thumb/ABNXxn0Xy-hyfIjRkWpanxMTLdU_H6ARpalSS_jyQiRoMssaQU8hl_Lhl7usjLhIFwU2rxW_GvKyWhrNNlgNli28ApWH_ufd39gYg7DkvV6pCes2VuFeqAymC8uSrRIlFrC4Q8-oXKanUZ9906ACugF_hzp2KWIZ-0ZvHRObA0jBfWWeCfkkbDNF7UB2tYphcnlVZF08xc_Ywonu-1sgJwcExeAkU678rn-wshsK2OoajuNSpnXeFiaAz71Bh7URKs8aEiMzl0bj3D_9w91lIZt5TQAA2z_y4jotUXT54KxkZ557p1T7U_EpUmHJh2f5iVaEqm_9yatzalfB-YUIOfIZcMFrfILeU3rzKQTyiQE2Sw/p.png?fv_content=true&size_mode=5`,
-                        'https://previews.dropbox.com/p/thumb/ABMp5PPVnPwHPp13ZAxw3vRyjlNk-TPlkSKOS5mv1Ze62HFRRsM90nOmIHH1fbLkqk-TgbYjl1YAxjny1FGNwxNq3Eg2J-CqpQcPMurMuJYKNNWGBtHghPuCgMdwHjvHjAJjZjMKAR7On8kGZR7FGryad1buwGi9II6uOzVjDiBS-M6ZMBf41tnRTPLQ5gokpIaySm6fBGRPCxGVn2lKgX7XkwnSHqZXEpS6qTBPN8j5h2V5IMAvbS6h-bBTjOMweZkqgrsjwpgrQjef5z63-5HSts7Ei9mY7gCACZhbY4NIsRDDuCKAE0yw_QWdovpJ4seocOLrMs6NBOkaaCL0o05S_f9s1xO39NSegQ1mhJgzdA/p.png?size=2048x1536&size_mode=3']} />
+ps: le code est en open-source sur mon Github`} theLink={'https://geek--blog.herokuapp.com/SignUp'} Images={[GeekBlog1, GeekBlog2, GeekBlog3]} />
                 <OneStuffsContainer parentCallback={clickOnWork} theId="Sy-Portfolio" title="Baba Sy Portfolio" gotALink={false} description="Ceci est le portfolio pour un artiste plasticien le site a une architecture simple comportent la biographie , présentation des oeuvres et présentation de l'atelier." theLink={'test'} Images={[
                     `https://previews.dropbox.com/p/thumb/ABMYGnWzy3OWkT8hiLyKHM0-o9eY85aFcDZiw_9HvrOZ6AORyMAygB6YtZPiCHnh85fMwC4hjPt3ODNVL4D4Xjms3RrdTlinGpCKwCQwWwLrStR4WgT0ZeCpG3e5fFSqFSF2W6z4mWGmm7QvO22oX8t_iecQ2MW4YdRWm2zbNIpmrfFRoD6NbrczN5KjCzRioLvqAZrWA_zQEeWe0t722R-G7UlN9M6OyKbbn9a4dnr-IYJpd_LmEcwRABX3_tf-C--4txZdCh12fmCkF6hNDhUw3YfbYqAJvZpFUVhvMGKutvFrG0MmGk_hAAGxKpM2d7lZkI9IHYZ6J2GQD4ANo2QqzSjpH3ClGmnh7Z30Fb-VIw/p.png?fv_content=true&size_mode=5`,
                     `https://previews.dropbox.com/p/thumb/ABNvbzaKlklUIErReznMTWaEKuzsfKIp9YoZwoqqczvNUJnVSysCQfvbfAzi79uEiAgvXg_q52NZQLQ16S2Floh4M5tQQ4vYoC1s0hiBpkEsFmtkewK6aQxkEurhKuPDrQmXESIhhCjs_cCBlfLZROgp_R0mwvw1S6lZYI2c6gKWoAXjYflMI_nUvrsb5vDSGosmpTN1vhzzTmTdDvlDmmDHv4NE3Yj93emYlktnzaHA5C62QiU5jYz7FFumHQOMOKTyi9mWgts-uViCOi2zMbOm3jWyLlT45gp-hdhhOpOb5VRmcDufAB3kfM36yL2apIkls0YOHT4AjLZNAtsyyr-1FV3GLdEvfUGMOlZAWuEZDR1K47C4T70nPnZLK_aGl-0zeJQmPYdl46cJNj1l-zOf5mtQR-88jYQbIpdwXHDm0A/p.png?size=2048x1536&size_mode=3`,
